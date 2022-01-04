e83f5c174e3298d250557eb2bbcbced7b73faa6e
Deymer Hernández <deymerh@hotmail.com>
2022-01-04 16:21:57 -0500

    Change theme based in configuration of system operative

--- src/context/theme/ThemeContext.tsx
+++ src/context/theme/ThemeContext.tsx
@@ -1,5 +1,7 @@
-import React, { createContext, useReducer } from 'react';
-import { lightheme, ThemeReducer, ThemeState } from './ThemeReducer';
+import React, { createContext, useEffect, useReducer } from 'react';
+import { Appearance, AppState } from 'react-native';
+import { useColorScheme } from 'react-native';
+import { lightheme, ThemeReducer, ThemeState, darktheme } from './ThemeReducer';
 
 interface ThemContextProps {
   theme: ThemeState;
@@ -10,7 +12,30 @@ interface ThemContextProps {
 export const ThemeContext = createContext({} as ThemContextProps);
 
 export const ThemeProvider = ({ children }: any) => {
-  const [theme, dispatch] = useReducer(ThemeReducer, lightheme);
+  const colorScheme = useColorScheme();
+  const [theme, dispatch] = useReducer(
+    ThemeReducer,
+    (Appearance.getColorScheme() === 'dark')
+      ? darktheme
+      : lightheme
+  );
+
+  useEffect(() => {
+    AppState.addEventListener('change', (status) => {
+      if (status === 'active') {
+        (Appearance.getColorScheme() === 'light')
+          ? setLightTheme()
+          : setDarkTheme()
+      }
+    })
+  }, []);
+
+  // useEffect(() => {
+  //   (colorScheme === 'light')
+  // ? setLightTheme()
+  // : setDarkTheme()
+  // }, [colorScheme]);
+
   const setLightTheme = () => {
     dispatch({ type: 'set_light_theme' });
   };
